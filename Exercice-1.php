abcc52340397d7fb370775435ac3f655b2f6b18a
AL DEIRI Mohamad <mhdderi93@gmail.com>
2022-12-13 09:23:32 +0100

    Ajout texte énoncé exo 1

--- Exercice-1.php
+++ Exercice-1.php
@@ -1,7 +1,7 @@
 <h1 style="color: #00FF00;  ">
     Exercice 1
 </h1>
-<p style="color: #229954; ">Soit la phrase «Notre formation DL commence aujourd'hui».Ecrire un algorithme permettant de compter le nombre de caractèrescontenus dans cette phrase (espaces inclus).</p>
+<p style="color: #229954; ">!!Soit la phrase «Notre formation DL commence aujourd'hui».Ecrire un algorithme permettant de compter le nombre de caractèrescontenus dans cette phrase (espaces inclus).</p>
 <hr>
 
 <?php
